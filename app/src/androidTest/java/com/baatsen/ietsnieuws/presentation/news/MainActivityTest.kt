ac26fb84812b0e036c39b65031aac63ae3d5b3db
Gilles Baatsen <gilles.baatsen@moneyou.nl>
2018-12-14 14:07:58 +0100

    fix refresh bug

---
+++ app/src/androidTest/java/com/baatsen/ietsnieuws/presentation/news/MainActivityTest.kt
@@ -0,0 +1,13 @@
+package com.baatsen.ietsnieuws.presentation.news
+
+
+import android.support.test.filters.LargeTest
+import android.support.test.runner.AndroidJUnit4
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class MainActivityTest {
+
+  //todo espresso tsts here
+}
