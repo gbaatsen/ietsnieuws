381ab182c26f1e0fe08a9b115a84ad4cdaf3050e
Gilles Baatsen <gilles@baatsen.nl>
2020-06-14 12:31:16 +0200

    update depedencies

--- app/src/main/java/com/baatsen/ietsnieuws/data/service/NewsService.kt
+++ app/src/main/java/com/baatsen/ietsnieuws/data/service/NewsService.kt
@@ -5,6 +5,7 @@ import com.baatsen.ietsnieuws.data.model.SourcesJson
 import io.reactivex.Observable
 import retrofit2.http.GET
 import retrofit2.http.Header
+import retrofit2.http.Headers
 import retrofit2.http.Query
 
 interface NewsService {
